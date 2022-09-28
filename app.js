b2770ecb89135a16375b93be3b03d8bf5ceca3aa
Linn Benjamin <beli21lh@student.ju.se>
2022-09-28 13:53:26 +0200

    added database

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
-const dummyData = require('./dummy-data')
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+const data = require('./data.js') 
+const sqlite3 = require('sqlite3')
+
+const db = new sqlite3.Database('Users/linns/database.db')
 
 const app = express()
 
@@ -16,8 +19,95 @@ app.get('/', function(request, response){
   response.render('home.hbs')
 })
 
-app.get('/', function(request, response){
+app.get('/projects', function(request, response){
+
+    const model = {
+      projects: data.projects
+    }
+
+    response.render('projects.hbs', model)
+
+  })
+
+
+  //GET /projects/id
+  app.get("/projects/:id", function(request, response){
+
+    const id = request.params.id
+
+    const project = data.projects.find(p => p.id == id)
+
+    const model = {
+      project: project,
+    }
+
+    response.render('project.hbs', model)
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+  app.get('/about', function(request, response){
     response.render('about.hbs')
+  })
+
+  app.get('/blog', function(request, response){
+    response.render('blog.hbs')
+  })
+
+  app.get('/contact', function(request, response){
+    response.render('contact.hbs')
+  })
+
+  app.get('/faq', function(request, response){
+    response.render('faq.hbs')
+  })
+
+
+
+
+
+
+
+
+
+  //*HAVENT IMPLEMENTED THIS YET*/
+  app.get('/secret-page', function(){
+    
+    if(isLoggedIn = true){
+      // Send back secret page
+
+    }else{
+      //Send back error
+
+    }
+  })
+
+  let isLoggedIn = false
+
+
+  app.post("/login", function(request, response){
+
+    const username = request.body.username
+    const password = request.body.password
+
+    if(username == ADMIN_USERNAME && password == ADMIN_PASSWORD){
+
+    }else{
+
+      const model = {
+        failedToLogin: true
+      }
+    }
+
   })
 
 app.listen(8080)
---
+++ data.js
@@ -0,0 +1,18 @@
+exports.projects = [{
+    id: 1,
+    title: "Post one",
+    difficulty: 2
+
+},
+
+exports.projects = {
+    id: 2,
+    title: "Post two",
+    difficulty: 4
+},
+
+exports.projects = {
+    id: 3,
+    title: "Post three",
+    difficulty: 5
+}]
