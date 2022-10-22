96f94c616464ca78f6e2274faca8a05b62a5d461
Linn Benjamin <beli21lh@student.ju.se>
2022-10-23 00:59:11 +0200

    Update app.js

--- app.js
+++ app.js
@@ -575,4 +575,11 @@ app.post("/logout", function(request, response){
   response.redirect("/")
 })
 
-app.listen(8080)
+
+app.listen(process.env.PORT);
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
